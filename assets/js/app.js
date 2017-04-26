f70d671b786cfa9678d46731c9b047f20ba01114
indiaaeevans <indiaaeevans@gmail.com>
2017-04-25 20:10:44 -0400

    fixed error
    Mixed Content: The page at 'https://bwright1114.github.io/Giphy-API/' was loaded over HTTPS, but requested an insecure XMLHttpRequest endpoint 'http://api.giphy.com/v1/gifs/search?q=Whitney%20Houston&api_key=dc6zaTOxFJmzC&limit=8'. This request has been blocked; the content must be served over HTTPS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@
 
     function ajaxRequest(topic) {
       // Constructing a URL to search Giphy for the topic selected
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         topic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       // Performing our AJAX GET request
